4f8b8c3035778fecd2a4b11740492642faf80b53
pruebabenja <alvarssjosefina@gmail.com>
2024-06-01 17:53:58 +1300

    Update info-creador.js

--- plugins/info-creador.js
+++ plugins/info-creador.js
@@ -11,8 +11,8 @@ let bio = biografia.status?.toString() || 'Sin Biografía'
 let biobot = biografiaBot.status?.toString() || 'Sin Biografía'
 
 await conn.sendContactArray(m.chat, [
-[devnum, `${await conn.getName('59168683798'+'@s.whatsapp.net')}`, `🍭 Creador`, dev, email, `🇧🇴 Bolivia`, `https://www.youtube.com/@Azami_YT`, bio],
-[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `🍧 CuriosityBot-MD`, `📵 No Hacer Spam`, email, `🇲🇽 México`, `https://github.com/AzamiJs/CuriosityBot-MD`, biobot]
+[devnum, `${await conn.getName('51936732723'+'@s.whatsapp.net')}`, `Creador`, dev, email, `Peru`, ``, bio],
+[`${conn.user.jid.split('@')[0]}`, `${await conn.getName(conn.user.jid)}`, `𝑯𝒚𝒑𝒆𝒓𝑿 𝑩𝒐𝒕`, `📵 No Hacer Spam`, email, `Peru`, ``, biobot]
 ], m)
 
 }
